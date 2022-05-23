12af67ead6571eeefee6b4bb21719b9d1dd55a25
FtobarV64 <ftobarv@gmail.com>
2022-05-23 16:11:52 -0400

    modify books_controller, model book, views index and applications, config routes commit

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,12 @@
 class BooksController < ApplicationController
   def index
   end
+
+  def show
+    @books = Book.all
+  end
+
+  def create
+    @book = Book.create(title: params[:title], author: params[:author], lend_at: params[:lend], returned_at: params[:returned])
+  end
 end
